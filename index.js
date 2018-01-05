d39d1834231dec4b229d4364ff477d2b988fd20c
zhengx <313478450@qq.com>
2018-01-05 22:14:30 +0800

    支持vue热替换

--- index.js
+++ index.js
@@ -190,206 +190,70 @@ exports = module.exports = function (options) {
         plugins: [__dirname + "/node_modules/babel-plugin-transform-decorators-legacy"]
     };
     if (!options["vuehot"]) {
-    async.map(jsCompileList, function (jsCompileItem, callback) {
-        let rebuildCompile = false;
-        let contextPath = path.join(global.staticDirectory, global.srcPrefix, 'js');
-        let staticFilesSourceDir = path.join(global.staticDirectory, global.srcPrefix);
-        let entryPath = './' + jsCompileItem.path.replace(utils.normalizePath(contextPath), '');
-        let config = {
-            context: contextPath,
-            entry: entryPath,
-            plugins: [
-                new webpack.DefinePlugin({
-                    __DEVTOOLS__: options.preact ? true : false
-                })
-            ],
-            output: {
-                path: path.join(global.staticDirectory, global.deployPrefix, 'js', utils.normalizePath(path.dirname(jsCompileItem.path)).replace(utils.normalizePath(contextPath), '')),
-                filename: "bundle.js",
-                chunkFilename: "[name].bundle.js",
-                publicPath: utils.normalizePath(path.join(global.sfPrefix, utils.normalizePath(path.join(global.deployPrefix, 'js', utils.normalizePath(path.dirname(jsCompileItem.path)).replace(utils.normalizePath(contextPath), ''))), '/'))
-            }
-        };
-
-        config.externals = {
-            "react": "React",
-            "react-dom": "ReactDOM",
-            "redux": "Redux",
-            "react-redux": "ReactRedux",
-            "react-router": "ReactRouter",
-            "react-router-dom": "ReactRouterDOM",
-            "preact-redux": "preactRedux",
-            "immutable": "Immutable",
-            "vue": "Vue",
-            "vue-router": "VueRouter",
-            "vuex": "Vuex"
-        };
-
-        config.module = {rules: utils.getRules()};
-        utils.extendConfig(config, commonConfig);
-
-        config.module.rules.push({
-            test: /\.(js|jsx)$/,
-            use: [{loader: 'babel-loader', options: JSON.stringify(babelSettings)}],
-            exclude: /(node_modules|bower_components)/,
-            include: [staticFilesSourceDir]
-        });
-
-        let compiler = webpack(config);
-        compiler.watch({
-            aggregateTimeout: 300,
-            poll: true
-        }, function (err, stats) {
-            if (err) {
-                throw err;
-            }
-
-            if (stats.hasErrors()) {
-                console.log('ERROR start ==============================================================');
-                console.log(stats.toString());
-                console.log('ERROR end   ==============================================================');
-            } else {
-                console.log(stats.toString());
-            }
-
-            if (rebuildCompile) {
-                console.log('rebuild complete!');
-                if (global.socket) {
-                    global.socket.emit("refresh", {"refresh": 1});
-                    console.log("files changed： trigger refresh...");
-                }
-
-                if (isHttps && global.httpsSocket) {
-                    global.httpsSocket.emit("refresh", {"refresh": 1});
-                    console.log("[https] files changed: trigger refresh...");
-                }
-            }
-
-            if (typeof callback == 'function') {
-                callback();
-            }
-
-            if (!rebuildCompile) {
-                rebuildCompile = true;
-                callback = null;
-            }
-        });
-    }, function (err) {
-        if (err) {
-            throw err;
-        }
-
-        //如果有需要使用react-hot-loader的入口JS
-        if (jsCompileListWithPureReact.length) {
-            let entryList = {};
-            let debugDomain = typeof options.debugDomain == 'string' ? options.debugDomain : 'local.wenwen.sogou.com';
-            jsCompileListWithPureReact.forEach(function (jsCompileItemWithPureReact) {
-                let entryKey = jsCompileItemWithPureReact.path.replace(utils.normalizePath(path.join(global.staticDirectory, 'src/')), 'sf/deploy/').replace('/main.js', '');
-                entryList[entryKey] = [
-                    'react-hot-loader/patch',
-                    'webpack-hot-middleware/client?reload=true',
-                    jsCompileItemWithPureReact.path
-                ];
-            });
-
+        async.map(jsCompileList, function (jsCompileItem, callback) {
+            let rebuildCompile = false;
+            let contextPath = path.join(global.staticDirectory, global.srcPrefix, 'js');
             let staticFilesSourceDir = path.join(global.staticDirectory, global.srcPrefix);
-
+            let entryPath = './' + jsCompileItem.path.replace(utils.normalizePath(contextPath), '');
             let config = {
-                devtool: "eval",
-                entry: entryList,
+                context: contextPath,
+                entry: entryPath,
                 plugins: [
-                    new webpack.HotModuleReplacementPlugin(),
-                    new webpack.NoEmitOnErrorsPlugin(),
                     new webpack.DefinePlugin({
                         __DEVTOOLS__: options.preact ? true : false
                     })
                 ],
                 output: {
-                    filename: "[name]/bundle.js",
+                    path: path.join(global.staticDirectory, global.deployPrefix, 'js', utils.normalizePath(path.dirname(jsCompileItem.path)).replace(utils.normalizePath(contextPath), '')),
+                    filename: "bundle.js",
                     chunkFilename: "[name].bundle.js",
-                    publicPath: '//' + debugDomain + ':' + global.hotPort + '/'
+                    publicPath: utils.normalizePath(path.join(global.sfPrefix, utils.normalizePath(path.join(global.deployPrefix, 'js', utils.normalizePath(path.dirname(jsCompileItem.path)).replace(utils.normalizePath(contextPath), ''))), '/'))
                 }
+            };
+
+            config.externals = {
+                "react": "React",
+                "react-dom": "ReactDOM",
+                "redux": "Redux",
+                "react-redux": "ReactRedux",
+                "react-router": "ReactRouter",
+                "react-router-dom": "ReactRouterDOM",
+                "preact-redux": "preactRedux",
+                "immutable": "Immutable",
+                "vue": "Vue",
+                "vue-router": "VueRouter",
+                "vuex": "Vuex"
             };
 
             config.module = {rules: utils.getRules()};
             utils.extendConfig(config, commonConfig);
-            config.externals = {};
 
             config.module.rules.push({
                 test: /\.(js|jsx)$/,
-                use: [{
-                    loader: 'babel-loader', options: JSON.stringify(babelSettings)
-                }],
+                use: [{loader: 'babel-loader', options: JSON.stringify(babelSettings)}],
+                exclude: /(node_modules|bower_components)/,
                 include: [staticFilesSourceDir]
             });
 
-            let express = require('express');
-            let app = express();
             let compiler = webpack(config);
-
-            app.use(require('webpack-dev-middleware')(compiler, {
-                publicPath: config.output.publicPath
-            }));
-
-            app.use(require('webpack-hot-middleware')(compiler));
-
-            app.get('*', function (req, res) {
-
-            });
-
-            app.listen(global.hotPort, function (err) {
+            compiler.watch({
+                aggregateTimeout: 300,
+                poll: true
+            }, function (err, stats) {
                 if (err) {
-                    return console.error(err);
+                    throw err;
                 }
 
-                console.log('Hot loader server start listening at http://' + debugDomain + ':' + global.hotPort + '/');
-            });
-        }
+                if (stats.hasErrors()) {
+                    console.log('ERROR start ==============================================================');
+                    console.log(stats.toString());
+                    console.log('ERROR end   ==============================================================');
+                } else {
+                    console.log(stats.toString());
+                }
 
-
-        if (!utils.hasArgument(process.argv, '--norefresh')) {
-            gulp.task('default', function () {
-                let watchFiles = [];
-
-                webappDirectoryList.forEach(function (item, index) {
-                    let webappViewSrcDir = item + '/src/main/webapp/WEB-INF/view/src/';
-
-                    if (isExpressProject) {
-                        let fileList = fs.readdirSync(item);
-
-                        fileList.forEach(function (filePath, index) {
-                            let stat = fs.statSync(path.join(item, filePath));
-
-                            //采用express推荐目录，只监控静态资源文件目录public以外的目录，同时忽略.svn等目录
-                            if (stat.isDirectory()) {
-                                if (filePath.toLowerCase() !== 'public' && filePath.indexOf('.') !== 0 && filePath.indexOf('node_modules') !== 0) {
-                                    if (filePath.toLowerCase() === 'views') {
-                                        watchFiles.push(path.join(item, filePath, "/src/**/*.js"));
-                                        watchFiles.push(path.join(item, filePath, "/src/**/*.jsx"));
-                                        watchFiles.push(path.join(item, filePath, "/src/**/*.html"));
-                                        watchFiles.push(path.join(item, filePath, "/src/**/*.tpl"));
-                                    } else if (filePath.toLowerCase() === 'bin') {
-                                        watchFiles.push(path.join(item, filePath, "/www"));
-                                    } else {
-                                        watchFiles.push(path.join(item, filePath, "/**/*.js"));
-                                    }
-                                }
-                            } else {
-                                if (path.extname(filePath) === '.js') {
-                                    watchFiles.push(path.join(item, filePath));
-                                }
-                            }
-                        });
-                    } else {
-                        watchFiles.push(path.join(webappViewSrcDir + "/**/*.vm"));
-                        watchFiles.push(path.join(webappViewSrcDir + "/**/*.html"));
-                        watchFiles.push(path.join(webappViewSrcDir + "/**/*.tpl"));
-                    }
-                });
-                watchFiles.push(cssCompileList);
-                console.log('watchFiles List: ');
-                console.log(watchFiles);
-                gulp.watch(watchFiles).on('change', function () {
+                if (rebuildCompile) {
+                    console.log('rebuild complete!');
                     if (global.socket) {
                         global.socket.emit("refresh", {"refresh": 1});
                         console.log("files changed： trigger refresh...");
@@ -397,19 +261,155 @@ exports = module.exports = function (options) {
 
                     if (isHttps && global.httpsSocket) {
                         global.httpsSocket.emit("refresh", {"refresh": 1});
-                        console.log("[https] file changed: trigger refresh...");
+                        console.log("[https] files changed: trigger refresh...");
                     }
-                });
-                utils.startWebSocketServer(isHttps);
-            });
+                }
 
-            gulp.start();
-        } else {
-            console.log('status: norefresh');
-        }
-    });
+                if (typeof callback == 'function') {
+                    callback();
+                }
+
+                if (!rebuildCompile) {
+                    rebuildCompile = true;
+                    callback = null;
+                }
+            });
+        }, function (err) {
+            if (err) {
+                throw err;
+            }
+
+            //如果有需要使用react-hot-loader的入口JS
+            if (jsCompileListWithPureReact.length) {
+                let entryList = {};
+                let debugDomain = typeof options.debugDomain == 'string' ? options.debugDomain : 'local.wenwen.sogou.com';
+                jsCompileListWithPureReact.forEach(function (jsCompileItemWithPureReact) {
+                    let entryKey = jsCompileItemWithPureReact.path.replace(utils.normalizePath(path.join(global.staticDirectory, 'src/')), 'sf/deploy/').replace('/main.js', '');
+                    entryList[entryKey] = [
+                        'react-hot-loader/patch',
+                        'webpack-hot-middleware/client?reload=true',
+                        jsCompileItemWithPureReact.path
+                    ];
+                });
+
+                let staticFilesSourceDir = path.join(global.staticDirectory, global.srcPrefix);
+
+                let config = {
+                    devtool: "eval",
+                    entry: entryList,
+                    plugins: [
+                        new webpack.HotModuleReplacementPlugin(),
+                        new webpack.NoEmitOnErrorsPlugin(),
+                        new webpack.DefinePlugin({
+                            __DEVTOOLS__: options.preact ? true : false
+                        })
+                    ],
+                    output: {
+                        filename: "[name]/bundle.js",
+                        chunkFilename: "[name].bundle.js",
+                        publicPath: '//' + debugDomain + ':' + global.hotPort + '/'
+                    }
+                };
+
+                config.module = {rules: utils.getRules()};
+                utils.extendConfig(config, commonConfig);
+                config.externals = {};
+
+                config.module.rules.push({
+                    test: /\.(js|jsx)$/,
+                    use: [{
+                        loader: 'babel-loader', options: JSON.stringify(babelSettings)
+                    }],
+                    include: [staticFilesSourceDir]
+                });
+
+                let express = require('express');
+                let app = express();
+                let compiler = webpack(config);
+
+                app.use(require('webpack-dev-middleware')(compiler, {
+                    publicPath: config.output.publicPath
+                }));
+
+                app.use(require('webpack-hot-middleware')(compiler));
+
+                app.get('*', function (req, res) {
+
+                });
+
+                app.listen(global.hotPort, function (err) {
+                    if (err) {
+                        return console.error(err);
+                    }
+
+                    console.log('Hot loader server start listening at http://' + debugDomain + ':' + global.hotPort + '/');
+                });
+            }
+
+
+            if (!utils.hasArgument(process.argv, '--norefresh')) {
+                gulp.task('default', function () {
+                    let watchFiles = [];
+
+                    webappDirectoryList.forEach(function (item, index) {
+                        let webappViewSrcDir = item + '/src/main/webapp/WEB-INF/view/src/';
+
+                        if (isExpressProject) {
+                            let fileList = fs.readdirSync(item);
+
+                            fileList.forEach(function (filePath, index) {
+                                let stat = fs.statSync(path.join(item, filePath));
+
+                                //采用express推荐目录，只监控静态资源文件目录public以外的目录，同时忽略.svn等目录
+                                if (stat.isDirectory()) {
+                                    if (filePath.toLowerCase() !== 'public' && filePath.indexOf('.') !== 0 && filePath.indexOf('node_modules') !== 0) {
+                                        if (filePath.toLowerCase() === 'views') {
+                                            watchFiles.push(path.join(item, filePath, "/src/**/*.js"));
+                                            watchFiles.push(path.join(item, filePath, "/src/**/*.jsx"));
+                                            watchFiles.push(path.join(item, filePath, "/src/**/*.html"));
+                                            watchFiles.push(path.join(item, filePath, "/src/**/*.tpl"));
+                                        } else if (filePath.toLowerCase() === 'bin') {
+                                            watchFiles.push(path.join(item, filePath, "/www"));
+                                        } else {
+                                            watchFiles.push(path.join(item, filePath, "/**/*.js"));
+                                        }
+                                    }
+                                } else {
+                                    if (path.extname(filePath) === '.js') {
+                                        watchFiles.push(path.join(item, filePath));
+                                    }
+                                }
+                            });
+                        } else {
+                            watchFiles.push(path.join(webappViewSrcDir + "/**/*.vm"));
+                            watchFiles.push(path.join(webappViewSrcDir + "/**/*.html"));
+                            watchFiles.push(path.join(webappViewSrcDir + "/**/*.tpl"));
+                        }
+                    });
+                    watchFiles.push(cssCompileList);
+                    console.log('watchFiles List: ');
+                    console.log(watchFiles);
+                    gulp.watch(watchFiles).on('change', function () {
+                        if (global.socket) {
+                            global.socket.emit("refresh", {"refresh": 1});
+                            console.log("files changed： trigger refresh...");
+                        }
+
+                        if (isHttps && global.httpsSocket) {
+                            global.httpsSocket.emit("refresh", {"refresh": 1});
+                            console.log("[https] file changed: trigger refresh...");
+                        }
+                    });
+                    utils.startWebSocketServer(isHttps);
+                });
+
+                gulp.start();
+            } else {
+                console.log('status: norefresh');
+            }
+        });
     } else {
-        //vue热替换 默认开启
+        //检测到--vuehot参数，启动Vue.js热加载
         if (jsCompileList.length) {
             let entryList = {};
             let debugDomain = options.debugDomain || 'local.baike.sogou.com';
