3fbee080313ffd2d376f00d89ef95f4a43a55d9a
Stanny2017 <dpxidian@gmail.com>
2020-02-04 12:27:34 +0800

    添加 common-library 文件监控服务逻辑

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const validate = require('./lib/validate');
 const workerFarm = require("worker-farm");
 const os = require('os');
 const chokidar = require("chokidar");
+const compileCommonLib = require('./compileCommonLib');
+
 global.srcPrefix = '/src/';
 global.deployPrefix = '/deploy/';
 global.localStaticResourcesPrefix = /\/sf/;
@@ -26,126 +28,134 @@ const workerOptions = process.platform === 'win32' ?
         maxConcurrentCallsPerWorker: 1
     } : { maxConcurrentWorkers: maxConcurrentWorkers };
 const workers = workerFarm(workerOptions, require.resolve('./webpack-compiler'));
+
 exports = module.exports = function (options) {
-    const { jsCompileList } = validate(options);
-    const commonConfig = {
-        cache: true,
-        resolve: {
-            modules: [
-                path.join(__dirname, "node_modules")
-            ],
-            extensions: ['.js', '.jsx'],
-            alias: options.preact ? {
-                'react': 'preact-compat',
-                'react-dom': 'preact-compat'
-            } : {}
-        },
-        resolveLoader: {
-            modules: [
-                path.join(__dirname, "node_modules")
-            ]
-        },
-        devtool: "inline-source-map",
-        mode: "development"
-    };
-    if (options.np) {//公用的客户端私有npm包需要从项目目录下查找依赖包
-        commonConfig.resolve.modules.push(path.join(options.staticFilesDirectory, '../node_modules'));
-    }
+    if (options.updateCommonLib) {
+        
+        compileCommonLib(options.updateCommonLib);
 
-    const _presets = [
-        __dirname + "/node_modules/@babel/preset-env"
-    ];
-
-    if (options.preact) {
-        _presets.push([__dirname + "/node_modules/@babel/preset-react", { "pragma": "h" }]);
     } else {
-        _presets.push(__dirname + "/node_modules/@babel/preset-react");
-    }
+        const { jsCompileList } = validate(options);
+        const commonConfig = {
+            cache: true,
+            resolve: {
+                modules: [
+                    path.join(__dirname, "node_modules")
+                ],
+                extensions: ['.js', '.jsx'],
+                alias: options.preact ? {
+                    'react': 'preact-compat',
+                    'react-dom': 'preact-compat'
+                } : {}
+            },
+            resolveLoader: {
+                modules: [
+                    path.join(__dirname, "node_modules")
+                ]
+            },
+            devtool: "inline-source-map",
+            mode: "development"
+        };
+        if (options.np) {//公用的客户端私有npm包需要从项目目录下查找依赖包
+            commonConfig.resolve.modules.push(path.join(options.staticFilesDirectory, '../node_modules'));
+        }
 
-    const babelSettings = {
-        cacheDirectory: true,
-        presets: _presets,
-        compact: false,
-        plugins: [
-            [__dirname + "/node_modules/@babel/plugin-proposal-decorators", { legacy: true }],
-            [__dirname + "/node_modules/@babel/plugin-proposal-class-properties", { "loose": false }],
-            __dirname + "/node_modules/@babel/plugin-syntax-dynamic-import",
-            __dirname + "/node_modules/@babel/plugin-syntax-import-meta"
-        ]
-    };
+        const _presets = [
+            __dirname + "/node_modules/@babel/preset-env"
+        ];
 
-    const externals = {
-        "react": "React",
-        "react-dom": "ReactDOM",
-        "redux": "Redux",
-        "react-redux": "ReactRedux",
-        "react-router": "ReactRouter",
-        "react-router-dom": "ReactRouterDOM",
-        "preact-redux": "preactRedux",
-        "immutable": "Immutable",
-        "preact": "preact",
-        "antd": "antd"
-    };
+        if (options.preact) {
+            _presets.push([__dirname + "/node_modules/@babel/preset-react", { "pragma": "h" }]);
+        } else {
+            _presets.push(__dirname + "/node_modules/@babel/preset-react");
+        }
 
-    const preact = !!options.preact;
-    const np = !!options.np;
-    const { staticDirectory, srcPrefix, deployPrefix, webappDirectoryList, cssCompileList, sfPrefix } = global;
-    let _leftCompileLen = jsCompileList.length;
-    for (let i = 0, len = jsCompileList.length; i < len; i++) {
-        const jsCompileItem = jsCompileList[i];
+        const babelSettings = {
+            cacheDirectory: true,
+            presets: _presets,
+            compact: false,
+            plugins: [
+                [__dirname + "/node_modules/@babel/plugin-proposal-decorators", { legacy: true }],
+                [__dirname + "/node_modules/@babel/plugin-proposal-class-properties", { "loose": false }],
+                __dirname + "/node_modules/@babel/plugin-syntax-dynamic-import",
+                __dirname + "/node_modules/@babel/plugin-syntax-import-meta"
+            ]
+        };
 
-        workers({ jsCompileItem, externals, commonConfig, babelSettings, preact, np, staticDirectory, srcPrefix, sfPrefix, deployPrefix, webappDirectoryList, cssCompileList, childId: i }, () => {
-            _leftCompileLen = _leftCompileLen - 1;
-            if (!_leftCompileLen) {
-                console.log(`**************** total compile time: ${new Date() - global.startCompile}ms ****************`);
+        const externals = {
+            "react": "React",
+            "react-dom": "ReactDOM",
+            "redux": "Redux",
+            "react-redux": "ReactRedux",
+            "react-router": "ReactRouter",
+            "react-router-dom": "ReactRouterDOM",
+            "preact-redux": "preactRedux",
+            "immutable": "Immutable",
+            "preact": "preact",
+            "antd": "antd"
+        };
 
-                if (!utils.hasArgument(process.argv, '--norefresh')) {
-                    let templateWatchFiles = [];
+        const preact = !!options.preact;
+        const np = !!options.np;
+        const { staticDirectory, srcPrefix, deployPrefix, webappDirectoryList, cssCompileList, sfPrefix } = global;
+        let _leftCompileLen = jsCompileList.length;
+        for (let i = 0, len = jsCompileList.length; i < len; i++) {
+            const jsCompileItem = jsCompileList[i];
 
-                    webappDirectoryList.forEach(function (item) {
-                        const webappViewSrcDir = options.np ? item : item + '/src/main/webapp/WEB-INF/view/src/';
+            workers({ jsCompileItem, externals, commonConfig, babelSettings, preact, np, staticDirectory, srcPrefix, sfPrefix, deployPrefix, webappDirectoryList, cssCompileList, childId: i }, () => {
+                _leftCompileLen = _leftCompileLen - 1;
+                if (!_leftCompileLen) {
+                    console.log(`**************** total compile time: ${new Date() - global.startCompile}ms ****************`);
 
-                        templateWatchFiles.push(path.join(webappViewSrcDir + "/**/*.vm"));
-                        templateWatchFiles.push(path.join(webappViewSrcDir + "/**/*.html"));
-                        templateWatchFiles.push(path.join(webappViewSrcDir + "/**/*.tpl"));
-                        templateWatchFiles.push(path.join(webappViewSrcDir + "/**/*.njk"));
-                    });
-                    templateWatchFiles = templateWatchFiles.concat(cssCompileList);
-                    console.log('templateWatchFiles List: ');
-                    console.log(templateWatchFiles);
-                    utils.startWebSocketServer();
-                    chokidar.watch(templateWatchFiles).on('change', () => {
-                        if (global.socket) {
-                            global.socket.emit("refresh", { "refresh": 1 });
-                            console.log("some files changed: trigger refresh...");
-                        }
-                    }).on('unlink', () => {
-                        if (global.socket) {
-                            global.socket.emit("refresh", { "refresh": 1 });
-                            console.log("some files deleted: trigger refresh...");
-                        }
-                    });
+                    if (!utils.hasArgument(process.argv, '--norefresh')) {
+                        let templateWatchFiles = [];
 
-                    const watcher = chokidar.watch(path.join(global.staticDirectory, global.deployPrefix));
+                        webappDirectoryList.forEach(function (item) {
+                            const webappViewSrcDir = options.np ? item : item + '/src/main/webapp/WEB-INF/view/src/';
 
-                    watcher.on("change", () => {
-                        if (global.socket) {
-                            global.socket.emit("refresh", { "refresh": 1 });
-                            console.log("some static files changed: trigger refresh...");
-                        }
-                    }).on("unlink", () => {
-                        if (global.socket) {
-                            global.socket.emit("refresh", { "refresh": 1 });
-                            console.log("some static files deleted: trigger refresh...");
-                        }
-                    });
-                } else {
-                    console.log('status: norefresh');
+                            templateWatchFiles.push(path.join(webappViewSrcDir + "/**/*.vm"));
+                            templateWatchFiles.push(path.join(webappViewSrcDir + "/**/*.html"));
+                            templateWatchFiles.push(path.join(webappViewSrcDir + "/**/*.tpl"));
+                            templateWatchFiles.push(path.join(webappViewSrcDir + "/**/*.njk"));
+                        });
+                        templateWatchFiles = templateWatchFiles.concat(cssCompileList);
+                        console.log('templateWatchFiles List: ');
+                        console.log(templateWatchFiles);
+                        utils.startWebSocketServer();
+                        chokidar.watch(templateWatchFiles).on('change', () => {
+                            if (global.socket) {
+                                global.socket.emit("refresh", { "refresh": 1 });
+                                console.log("some files changed: trigger refresh...");
+                            }
+                        }).on('unlink', () => {
+                            if (global.socket) {
+                                global.socket.emit("refresh", { "refresh": 1 });
+                                console.log("some files deleted: trigger refresh...");
+                            }
+                        });
+
+                        const watcher = chokidar.watch(path.join(global.staticDirectory, global.deployPrefix));
+
+                        watcher.on("change", () => {
+                            if (global.socket) {
+                                global.socket.emit("refresh", { "refresh": 1 });
+                                console.log("some static files changed: trigger refresh...");
+                            }
+                        }).on("unlink", () => {
+                            if (global.socket) {
+                                global.socket.emit("refresh", { "refresh": 1 });
+                                console.log("some static files deleted: trigger refresh...");
+                            }
+                        });
+                    } else {
+                        console.log('status: norefresh');
+                    }
                 }
-            }
+            });
+        }
+
+        process.on("exit", function () {
+            workerFarm.end(workers);
         });
     }
-    process.on("exit", function () {
-        workerFarm.end(workers);
-    });
 };
